7b674910b2e527ff0129cd32ffd55a033d3aa840
Billy <billygun27@gmail.com>
2018-02-28 02:00:39 +0700

    upload

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
+const path = require('path');
 var router = express.Router();
 
+var moment = require('moment-timezone');
+//moment().format();
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/*router.get('/', function(request, response, next) {
+  var ind = moment().tz("Asia/Jakarta")
+//console.log(moment.locale());         // en
+//console.log(moment().format('LT'));   // 11:34 AM
+  response.send(ind.format('l') +"clock"+ ind.format('HH:mm:ss'));
+});*/
+
+router.get('/upload', function(request, response, next) {
+  response.sendFile(path.join( __dirname  ,'../public/upload.html'));
 });
+
 
 module.exports = router;
